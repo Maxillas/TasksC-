2270b03058f181b0ddcce8da6a4f660cf968e352
Maxillas <maksanchutin@mail.ru>
2024-05-23 20:27:15 +0500

    new task

--- CScpp/task2_3.cpp
+++ CScpp/task2_3.cpp
@@ -9,6 +9,7 @@ std::pair<double, char const *> p = to_pair<1,2>(t);
  */
 
 #include <iostream>
+#include <functional>
 #include <tuple>
 
 template<int firstIndex, int secondIndex, typename... Tup>
@@ -54,3 +55,83 @@ constexpr double operator*(const Point &a, const Point &b)
 {
     return a.x * b.x + a.y * b.y;
 }
+
+/*
+
+Рассмотрим следующую функцию for_each.
+
+Определите переменную square_fun, хранящую безымянную функцию, которая возводит переданное число в квадрат.
+
+Пример:
+*/
+
+template<class F>
+void for_each_int(int * p, int * q, F f)
+{
+    for ( ; p != q; ++p )
+        f(*p);
+}
+
+// определение переменной
+auto square_fun = [](int & num){ num *= num;};
+
+/*Рассмотрим следующую функцию find_if:
+
+template<class F>
+int * find_if(int * p, int * q, F f)
+{
+    for ( ; p != q; ++p )
+        if (f(*p))
+            return p;
+    return q;
+}
+
+Определите переменную gen_finder, хранящую безымянную функцию, которая принимает массив значений типа int через два указателя и
+возвращает безымянную функцию, которая в свою очередь принимает значение типа int и проверяет, есть ли это значение в переданном массиве.
+
+Пример:
+
+int primes[5] = {2,3,5,7,11};
+
+int m[10] = {0,0,1,1,4,6,7,8,9,10};
+
+// first_prime будет указывать на число 7
+int * first_prime = find_if(m, m + 10, gen_finder(primes, primes + 5));
+*/
+
+template<class F>
+int * find_if(int * p, int * q, F f)
+{
+    for ( ; p != q; ++p )
+        if (f(*p))
+            return p;
+    return q;
+}
+
+auto gen_finder = [](int* arrayStart, int* arrayEnd){
+    auto foo = [arrayStart, arrayEnd](int inputNum){
+        int* start = arrayStart;
+        int* end = arrayEnd;
+        for (; start != end; ++start) {
+            if(inputNum == *start) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    return foo;};
+
+int primes[5] = {2,3,5,7,11};
+
+int m[10] = {0,0,1,1,4,6,7,8,9,10};
+
+// first_prime будет указывать на число 7
+int * first_prime = find_if(m, m + 10, gen_finder(primes, primes + 5));
+
+
+
+
+
+
+
