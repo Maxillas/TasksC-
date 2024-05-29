77412cb471ea6def861cefae7d6f79bc5df2cb91
Maxillas <maksanchutin@mail.ru>
2024-05-29 22:08:30 +0500

    addImpl metod realisation task3_3

--- CScpp/task3_3.cpp
+++ CScpp/task3_3.cpp
@@ -1,5 +1,16 @@
 #include <typeinfo>
 #include <typeindex>
+#include <map>
+#include <iostream>
+
+// базовый класс фигуры (полиморфный)
+struct Shape {  };
+
+// прямоугольник
+struct Rectangle : Shape {  };
+
+// треугольник
+struct Triangle : Shape {  };
 
 // Base - базовый класс иерархии
 // Result - тип возвращаемого значения мультиметода
@@ -13,19 +24,22 @@ struct Multimethod2
     // f - это функция или функциональный объект
     // принимающий два указателя на Base
     // и возвращающий значение типа Result
-    void addImpl( ... t1, ... t2, ... f )
+    void addImpl(std::type_info const &t1, std::type_info const &t2, Result (*foo)(Base* a, Base* b))
     {
-
+        std::pair<std::type_index, std::type_index> pair = std::make_pair(std::type_index(t1), std::type_index(t2));
+        m_fooContainer.emplace(pair, foo);
     }
 
     // проверяет, есть ли реализация мультиметода
     // для типов объектов a и b
     bool hasImpl(Base * a, Base * b) const
     {
+        // здесь проверить на комутативность. если ЕСТЬ пара нужная, то збс, если нет, то нет
         // возвращает true, если реализация есть
         // если операция коммутативная, то нужно
         // проверить есть ли реализация для b и а
-        return ...;
+       // return ...;
+        return true;
     }
 
     // Применяет мультиметод к объектам
@@ -34,6 +48,11 @@ struct Multimethod2
     {
         // возвращает результат применения реализации
         // мультиметода к a и b
-        return ...;
+       // return ...;
+        return true;
     }
+
+private:
+    std::map<std::pair<std::type_index, std::type_index>, Result (*)(Base* a, Base* b)> m_fooContainer;
+
 };
