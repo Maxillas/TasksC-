1510c0de1b1b3441e6dc76016fa12f2fab687506
Maxillas <maksanchutin@mail.ru>
2024-05-31 16:12:34 +0500

    task 3_3 solved

--- CScpp/main.cpp
+++ CScpp/main.cpp
@@ -4,70 +4,482 @@
 #include "task3_2.cpp"
 #include "task3_3.cpp"
 
+#include <iostream>
+#include <cstdint>
+
+
 using namespace std;
 
 void * foog(size_t a, char const* b) {return 0;};
 
-// функция для проверки пересечения двух прямоугольников
-bool is_intersect_r_r(Shape * a, Shape * b) {std::cout << "intersect_R_R" << std::endl; return true;};
-
-// функция для проверки пересечения прямоугольника и треугольника
-bool is_intersect_r_t(Shape * a, Shape * b) {std::cout << "intersect_R_T" << std::endl; return true;};
+/* Write your code here */
 
 
-int main()
-{
 
-    //print_values(std::cout, 0, 3.5, "Hello");
+struct Shape {
+    int32_t shape_;
+    constexpr Shape(int32_t shape) : shape_(shape) {}
+    virtual int32_t shapenum() { return this->shape_; };
+    virtual ~Shape() = default;
+};
 
-    //std::list<int> const l = {3,2,1};
-//    int len1 = max_increasing_len(l.begin(), l.end());
-//    if(len1 == 1) std::cout << "1 TRUE" << std::endl;
-//    else std::cout << "1 FALSE. Ожидается 1, получено " << len1 << std::endl;
-//    //
-//    std::list<int> const l2 = {7,8,9,4,5,6,1,2,3,4};
-//    size_t len2 = max_increasing_len(l2.begin(), l2.end()); // 4, соответствует подотрезку 1,2,3,4
-//    if(len2 == 4) std::cout << "2 TRUE" << std::endl;
-//    else std::cout << "2 FALSE. Ожидается 4, получено " << len2 << std::endl;
-//    //
-//    std::list<int> const l3 = {-3,-2,-1,0,0,1,2,3,4,5};
-//    size_t len3 = max_increasing_len(l3.begin(), l3.end()); // 6, соответствует подотрезку 0,1,2,3,4,5
-//    if(len3 == 6) std::cout << "3 TRUE" << std::endl;
-//    else std::cout << "3 FALSE. Ожидается 6, получено " << len3 << std::endl;
-//    //
-//    std::list<int> const l4 = {1,2,3};
-//    int len4 = max_increasing_len(l4.begin(), l4.end());
-//    if(len4 == 3) std::cout << "4 TRUE" << std::endl;
-//    else std::cout << "4 FALSE. Ожидается 3, получено " << len4 << std::endl;
-//    //
-//    std::list<int> const l5 = {};
-//    int len5 = max_increasing_len(l5.begin(), l5.end());
-//    if(len5 == 0) std::cout << "5 TRUE" << std::endl;
-//    else std::cout << "5 FALSE. Ожидается 0, получено " << len5 << std::endl;
-//    //
-//    std::list<int> const l6 = {1, 1, 1, 1, 1};
-//    int len6 = max_increasing_len(l6.begin(), l6.end());
-//    if(len6 == 1) std::cout << "6 TRUE" << std::endl;
-//    else std::cout << "6 FALSE. Ожидается 1, получено " << len6 << std::endl;
-
-    /********************************/
-    /* 3_3 */
+struct Point {
+    double x_;
+    double y_;
+    constexpr Point(double x, double y) : x_(x), y_(y) {}
+};
 
 
-    // функция для проверки пересечения двух прямоугольников
-    //bool is_intersect_r_r(Shape * a, Shape * b);
-
-    // функция для проверки пересечения прямоугольника и треугольника
-    //bool is_intersect_r_t(Shape * a, Shape * b);
-
-    // мультиметод для наследников Shape
-    // возращающий bool и являющийся коммутативным
-    Multimethod2<Shape, bool, true> is_intersect;
-
-    // добавляем реализацию мультиметода для двух прямоугольников
-    is_intersect.addImpl(typeid(Rectangle), typeid(Rectangle), is_intersect_r_r);
+struct Rectangle : Shape {
+    Point leftbottom_, righttop_;
+    constexpr Rectangle(double x1 = 0, double y1 = 0, double x2 = 1, double y2 = 1)
+        : Shape(4), leftbottom_(x1, y1), righttop_(x2, y2) {};
+};
 
 
+struct Triangle : Shape {
+    Point first_, second_, third_;
+    constexpr Triangle(double x1 = 0, double y1 = 0, double x2 = 6, double y2 = 6, double x3 = 12, double y3 = 0)
+        : Shape(3), first_(x1, y1), second_(x2, y2), third_(x3, y3) {}
+
+};
+
+bool is_both_same_shape(Shape* a, Shape* b) {
+    return( a->shapenum() == b->shapenum()) ? true : false;
+}
+
+
+bool is_both_rectangle(Shape* a, Shape* b) {
+    return (a->shapenum() == 4 && b->shapenum() == 4) ? true : false;
+}
+
+bool is_both_triangle(Shape* a, Shape* b) {
+    return (a->shapenum() == 3 && b->shapenum() == 3 )? true : false;
+}
+
+bool is_first_triangle_second_rectangle(Shape* a, Shape* b) {
+    return ((a->shapenum() == 3) && (b->shapenum() == 4)) ? true : false;
+}
+
+bool is_first_rectangle_second_triangle(Shape* a, Shape* b) {
+    return (a->shapenum() == 4 && b->shapenum() == 3 )? true : false;
+}
+
+
+bool is_one_triangle_another_rectangle(Shape* a, Shape* b) {
+    return ((a->shapenum() == 3 && b->shapenum() == 4) || ( a->shapenum() == 4 && b->shapenum() == 3 )) ? true : false;
+}
+
+bool test_defaultctor() {
+    std::cout << __FUNCTION__ << " result: ";
+    Shape* trg = static_cast<Shape*>(new Triangle());
+    Shape* rct = static_cast<Shape*>(new Rectangle());
+    delete trg;
+    delete rct;
+    return true;
+}
+
+
+
+
+
+bool test_multimethod_comm_one_triangle_another_rectangle() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, true> multimethod_obj;
+
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Triangle), is_one_triangle_another_rectangle);
+
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0]  = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: "  << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1]  = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: "  << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2]  = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3]= multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && is_done[1] && is_done[2] && !is_done[3] && !res[0] && res[1] && res[2] && !res[3];
+}
+
+bool test_multimethod_comm_first_triangle_second_rectangle() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, true> multimethod_obj;
+    auto a_foo_stdfunction = std::function<bool(Shape*, Shape*)>(is_first_triangle_second_rectangle);
+
+
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Triangle), a_foo_stdfunction);
+
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0] = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: " << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1] = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: " << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2] = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3] = multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && is_done[1] && is_done[2] && !is_done[3] && !res[0] && !res[1] && !res[2] && !res[3];
+}
+
+bool test_multimethod_comm_both_rectangle() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, true> multimethod_obj;
+
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Rectangle), is_both_rectangle);
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0] = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: " << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1] = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: " << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2] = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3] = multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && !is_done[1] && !is_done[2] && is_done[3] && !res[0] && !res[1] && !res[2] && res[3];
+}
+
+bool test_multimethod_noncomm_both_rectangle() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, false> multimethod_obj;
+
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Rectangle), is_both_rectangle);
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0] = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: " << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1] = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: " << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2] = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3] = multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && !is_done[1] && !is_done[2] && is_done[3] && !res[0] && !res[1] && !res[2] && res[3];
+}
+
+bool test_multimethod_noncomm_first_rectangle_second_triangle_twoinjections() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, false> multimethod_obj;
+
+
+
+    multimethod_obj.addImpl(typeid(Triangle), typeid(Rectangle), is_first_rectangle_second_triangle);
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Triangle), is_first_rectangle_second_triangle);
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0] = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: " << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1] = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: " << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2] = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3] = multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && is_done[1] && is_done[2] && !is_done[3] && !res[0] && !res[1] && res[2] && !res[3];
+}
+
+bool test_multimethod_noncomm_first_rectangle_second_triangle() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, false> multimethod_obj;
+
+
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Triangle), is_first_rectangle_second_triangle);
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0] = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: " << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1] = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: " << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2] = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3] = multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && !is_done[1] && is_done[2] && !is_done[3] && !res[0] && !res[1] && res[2] && !res[3];
+}
+
+bool test_multimethod_comm_first_triangle_second_rectangleAND_first_rectangle_second_triangle() {
+
+    std::cout << __FUNCTION__ << ' ';
+    Multimethod2<Shape, bool, true> multimethod_obj;
+
+    multimethod_obj.addImpl(typeid(Triangle), typeid(Rectangle), is_first_triangle_second_rectangle);
+    multimethod_obj.addImpl(typeid(Rectangle), typeid(Triangle), is_first_rectangle_second_triangle);
+
+    Shape* s1 = new Triangle();
+    Shape* s2 = new Rectangle();
+
+    bool is_done[4] = { false,false,false,false };
+    bool res[4] = { false, false, false, false };
+    if (multimethod_obj.hasImpl(s1, s1))
+    {
+
+        res[0] = false; res[0] = multimethod_obj.call(s1, s1);
+
+        is_done[0] = true;
+
+    }
+    std::cout << std::endl << '\t' << "Triangle  &  Triangle: " << multimethod_obj.hasImpl(s1, s1) << " ;is called: " << is_done[0] << " ;result: " << res[0] << std::endl;
+    if (multimethod_obj.hasImpl(s1, s2))
+    {
+
+        res[1] = false; res[1] = multimethod_obj.call(s1, s2);
+
+        is_done[1] = true;
+
+    }
+    std::cout << '\t' << "Triangle  & Rectangle: " << multimethod_obj.hasImpl(s1, s2) << " ;is called: " << is_done[1] << " ;result: " << res[1] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s1))
+    {
+
+        res[2] = false; res[2] = multimethod_obj.call(s2, s1);
+
+        is_done[2] = true;
+
+    }
+    std::cout << '\t' << "Rectangle &  Triangle: " << multimethod_obj.hasImpl(s2, s1) << " ;is called: " << is_done[2] << " ;result: " << res[2] << std::endl;
+    if (multimethod_obj.hasImpl(s2, s2))
+    {
+
+        res[3] = false; res[3] = multimethod_obj.call(s2, s2);
+
+        is_done[3] = true;
+
+    }
+    std::cout << '\t' << "Rectangle & Rectangle: " << multimethod_obj.hasImpl(s2, s2) << " ;is called: " << is_done[3] << " ;result: " << res[3] << std::endl;
+    delete s1;
+    delete s2;
+    return !is_done[0] && is_done[1] && is_done[2] && !is_done[3] && !res[0] && res[1] && res[2] && !res[3];
+}
+
+
+int main() {
+    //std::cout << test_defaultctor() << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_comm_one_triangle_another_rectangle() << std::endl << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_comm_first_triangle_second_rectangle() << std::endl << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_comm_both_rectangle() << std::endl << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_noncomm_both_rectangle() << std::endl << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_noncomm_first_rectangle_second_triangle_twoinjections() << std::endl << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_noncomm_first_rectangle_second_triangle() << std::endl << std::endl;
+    std::cout << std::endl << "check order test, because test on the site guessed only one implementation, and guess, that you can have different functions for different shape even with commutative." << std::endl;
+    std::cout << "Is it has to be like this: " << test_multimethod_comm_first_triangle_second_rectangleAND_first_rectangle_second_triangle() << std::endl << std::endl;
+
+    std::cout << "ended" << std::endl;
     return 0;
 }
 
--- CScpp/task3_3.cpp
+++ CScpp/task3_3.cpp
@@ -1,16 +1,8 @@
 #include <typeinfo>
 #include <typeindex>
 #include <map>
+#include <functional>
 #include <iostream>
-
-// базовый класс фигуры (полиморфный)
-struct Shape {  };
-
-// прямоугольник
-struct Rectangle : Shape {  };
-
-// треугольник
-struct Triangle : Shape {  };
 
 // Base - базовый класс иерархии
 // Result - тип возвращаемого значения мультиметода
@@ -24,7 +16,10 @@ struct Multimethod2
     // f - это функция или функциональный объект
     // принимающий два указателя на Base
     // и возвращающий значение типа Result
-    void addImpl(std::type_info const &t1, std::type_info const &t2, Result (*foo)(Base* a, Base* b))
+    void addImpl(std::type_info const &t1,
+                 std::type_info const &t2,
+                 std::function<Result(Base* a, Base* b)> foo)
+                 //Result (*foo)(Base* a, Base* b))
     {
         std::pair<std::type_index, std::type_index> pair = std::make_pair(std::type_index(t1), std::type_index(t2));
         m_fooContainer.emplace(pair, foo);
@@ -34,25 +29,57 @@ struct Multimethod2
     // для типов объектов a и b
     bool hasImpl(Base * a, Base * b) const
     {
+        std::pair<std::type_index, std::type_index> pair_A =
+        std::make_pair(std::type_index(typeid(*a)),
+                       std::type_index(typeid(*b)));
+
+
+        if(Commutative) {
+            std::pair<std::type_index, std::type_index> pair_B =
+            std::make_pair(std::type_index(typeid(*b)),
+                           std::type_index(typeid(*a)));
+
+            if(m_fooContainer.find(pair_A) != m_fooContainer.end() ||
+               m_fooContainer.find(pair_B) != m_fooContainer.end()) {
+                return true;
+            }
+            return false;
+        }
+        if(m_fooContainer.find(pair_A) != m_fooContainer.end()){
+            return true;
+        }
+        return false;
         // здесь проверить на комутативность. если ЕСТЬ пара нужная, то збс, если нет, то нет
         // возвращает true, если реализация есть
         // если операция коммутативная, то нужно
         // проверить есть ли реализация для b и а
-       // return ...;
-        return true;
     }
 
     // Применяет мультиметод к объектам
     // по указателям a и b
     Result call(Base * a, Base * b) const
     {
+        std::pair<std::type_index, std::type_index> pair_A =
+        std::make_pair(std::type_index(typeid(*a)),
+                       std::type_index(typeid(*b)));
+
+
+        std::pair<std::type_index, std::type_index> pair_B =
+        std::make_pair(std::type_index(typeid(*b)),
+                       std::type_index(typeid(*a)));
+
+        if(m_fooContainer.find(pair_A) != m_fooContainer.end()) {
+            return m_fooContainer.at(pair_A)(a, b);
+        } else if (m_fooContainer.find(pair_B) != m_fooContainer.end() && Commutative) {
+            return m_fooContainer.at(pair_B)(b, a);
+        }
         // возвращает результат применения реализации
-        // мультиметода к a и b
-       // return ...;
-        return true;
+        // мультиметод
     }
 
 private:
-    std::map<std::pair<std::type_index, std::type_index>, Result (*)(Base* a, Base* b)> m_fooContainer;
+    std::map<std::pair<std::type_index, std::type_index>, std::function<Result(Base*, Base*)>> m_fooContainer;
 
+    //Result (*)(Base* a, Base* b)
+    //std::function<Result(Base* a, Base* b)>
 };
