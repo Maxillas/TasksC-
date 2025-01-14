dfcdad4982509ee50a54b9f463d6fb59ad48a695
Maxillas <maksanchutin@mail.ru>
2025-01-14 18:44:20 +0500

    add: fuzzing tests

--- StringSorter/sortertests.h
+++ StringSorter/sortertests.h
@@ -1,13 +1,25 @@
 #ifndef SORTERTESTS_H
 #define SORTERTESTS_H
 
-class SorterTests
-{
-public:
-    SorterTests();
-    bool test1();
-    bool test2();
-    bool test3();
-};
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "sorter.h"
+#include <cstdint>
+
+// Функция фуззинга
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    try {
+        // Преобразуем данные в строку
+        std::string input(reinterpret_cast<const char*>(data), size);
+        RunProjectWithInput(input);  // Передаем данные в проект
+    } catch (...) {
+        // Ловим любые исключения для предотвращения крашей тестера
+        std::cerr << "Exception caught!" << std::endl;
+    }
+
+    return 0;  // Успешное завершение
+}
 
 #endif // SORTERTESTS_H
+
