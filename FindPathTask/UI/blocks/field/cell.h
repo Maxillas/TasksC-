d6070d28ba050498b16d77b15d3aad8a6699e9c7
Maxillas <maksanchutin@mail.ru>
2025-01-17 15:42:23 +0500

    add: brush for cell

--- FindPathTask/UI/blocks/field/cell.h
+++ FindPathTask/UI/blocks/field/cell.h
@@ -2,6 +2,7 @@
 #define CELL_H
 
 #include <QGraphicsItem>
+#include <QBrush>
 #include "../../../Core/cell_controller.h"
 
 class Cell : public QGraphicsRectItem
@@ -24,6 +25,7 @@ private:
         emit controller.onCellPressed();
         qDebug() << "Mouse pressed on CustomRectItem at:" << row << ".." << column;
         qDebug() << "isWall = " << isWall;
+        setBrush(QBrush(Qt::black));
 
     }
 };
