3d1d8897acda84240a63136d979f5d0fd2e0fdda
Maxillas <maksanchutin@mail.ru>
2025-01-17 16:18:21 +0500

    add: text in cell

--- FindPathTask/UI/blocks/field/cell.h
+++ FindPathTask/UI/blocks/field/cell.h
@@ -3,13 +3,18 @@
 
 #include <QGraphicsItem>
 #include <QBrush>
+#include <QFont>
 #include "../../../Core/cell_controller.h"
 
 class Cell : public QGraphicsRectItem
 {
 public:
     Cell(qreal x, qreal y, qreal width, qreal height, QGraphicsItem* parent = nullptr)
-        : QGraphicsRectItem(x, y, width, height, parent) {}
+        : QGraphicsRectItem(x, y, width, height, parent)
+    {
+        m_textItem = new QGraphicsTextItem(this);
+        m_textItem->setDefaultTextColor(Qt::black);
+    }
 
     bool isWall = false;
     bool visited = false;
@@ -21,12 +26,38 @@ public:
 
 private:
 
+    QGraphicsTextItem* m_textItem;
+
+    void centerText() {
+        QRectF rect = this->rect();
+        QRectF textRect = m_textItem->boundingRect();
+        m_textItem->setPos(rect.center() - textRect.center());
+    }
+
+    void updateFontSize() {
+        QRectF rect = this->rect();
+        qreal minSide = qMin(rect.width(), rect.height());
+        int fontSize = static_cast<int>(minSide * 0.5);
+
+        QFont font = m_textItem->font();
+        font.setPointSize(fontSize);
+        m_textItem->setFont(font);
+    }
+
+    void setCellText(QString text) {
+        m_textItem->setPlainText(text);
+        updateFontSize();
+        centerText();
+    };
+
+
     void mousePressEvent(QGraphicsSceneMouseEvent* event) override {
         emit controller.onCellPressed();
         qDebug() << "Mouse pressed on CustomRectItem at:" << row << ".." << column;
         qDebug() << "isWall = " << isWall;
-        setBrush(QBrush(Qt::black));
+        setCellText("2");
 
+        //setBrush(QBrush(Qt::black));
     }
 };
 
